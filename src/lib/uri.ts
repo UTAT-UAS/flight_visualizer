c090905860127fa745694c5241d38cc3dd9dc577
Benjamin Low <benlow999@gmail.com>
2026-05-11 03:41:25 +0000

    feat: github pages hosting

--- src/lib/uri.ts
+++ src/lib/uri.ts
@@ -1,3 +1,4 @@
-const HOST = "localhost";
+const urlParams = typeof window !== 'undefined' ? new URLSearchParams(window.location.search) : new URLSearchParams();
+export const HOST = urlParams.get('host') || "localhost";
 export const SIGNALLING_SERVER = `ws://${HOST}:8443`;
 export const ROS_BRIDGE = `ws://${HOST}:8080`;
